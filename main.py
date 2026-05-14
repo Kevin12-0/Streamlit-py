7f6877dfcc93f3f2d64fdc06a1e6c587e0ad564f
Kevin Lopez <1719110155@uetctulancingo.ude.mx>
2026-05-14 17:47:12 -0600

    Primeros pasas en streamlit

---
+++ main.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.title("Hola, Yo soy Kevin")  # titulo
+st.header("esto es un header")  # header
+st.subheader("Esto es un subheader")  # sub header
+st.text("Esto es un Texto")  # texto plano
+
+# Mas elementos para crear una pagina web
+# Se puede usar directamente el lenguaje Markdown
+# Es el mismo lenguaje que se ultilisa para hacer los README.md
+
+st.markdown("# Turtle Code")
+st.markdown("## Turtle Code")
+st.markdown("### Turtle Code")
+st.markdown("**Turtle** Code")
+st.markdown("*Turtle* Code")
